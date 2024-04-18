c28aa14c23e1d2fc64abf00667c3ca3ad49f0531
nivance <limingjian110@163.com>
2024-04-18 11:25:21 +0800

    update StockHisPriceSpider.java

--- src/main/java/org/whatever/stockspider/processor/zh/StockHisPriceProcessor.java
+++ src/main/java/org/whatever/stockspider/processor/zh/StockHisPriceProcessor.java
@@ -1,6 +1,5 @@
 package org.whatever.stockspider.processor.zh;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -8,7 +7,7 @@ import java.util.Objects;
 import org.springframework.stereotype.Component;
 import org.whatever.stockspider.db.entity.DayPrice;
 import org.whatever.stockspider.processor.MyPageProcessor;
-import org.whatever.stockspider.util.DateUtil;
+import org.whatever.stockspider.util.CommonUtil;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
@@ -38,34 +37,10 @@ public class StockHisPriceProcessor extends MyPageProcessor {
         }
         List<DayPrice> dayPrices = new ArrayList<>();
         for (int i = 0; i < klinesArray.size(); i++) {
-            dayPrices.add(toObject(code, name, klinesArray.getString(i)));
+            dayPrices.add(CommonUtil.toObject(code, name, klinesArray.getString(i)));
         }
         page.putField("dataList", dayPrices);
     }
 
-    /**
-     * 将字符串转换为Kline对象
-     *
-     * @param data 示例数据：2000-11-27,7.38,7.61,7.73,7.28,49762,37562000.00,6.06,2.56,0.19,0.00
-     * @return
-     */
-    private DayPrice toObject(String code, String name, String data) {
-        String[] split = data.split(",");
-        DayPrice dayPrice = new DayPrice();
-        dayPrice.setCode(code);
-        dayPrice.setName(name);
-        dayPrice.setTradingDate(DateUtil.formatDate(split[0], DateUtil.FORMAT_3).toDate());
-        dayPrice.setOpeningPrice(new BigDecimal(split[1]));
-        dayPrice.setClosingPrice(new BigDecimal(split[2]));
-        dayPrice.setPeakPrice(new BigDecimal(split[3]));
-        dayPrice.setBottomPrice(new BigDecimal(split[4]));
-        dayPrice.setTradingVolume(Integer.valueOf(split[5]));
-        dayPrice.setTradingAmount(new BigDecimal(split[6]));
-        dayPrice.setAmplitudeRate(new BigDecimal(split[7]));
-        dayPrice.setChangeRate(new BigDecimal(split[8]));
-        dayPrice.setChangeAmount(new BigDecimal(split[9]));
-        dayPrice.setTurnoverRate(new BigDecimal(split[10]));
-        return dayPrice;
-    }
 
 }
--- src/main/java/org/whatever/stockspider/spider/zh/StockHisPriceSpider.java
+++ src/main/java/org/whatever/stockspider/spider/zh/StockHisPriceSpider.java
@@ -1,13 +1,15 @@
 package org.whatever.stockspider.spider.zh;
 
+import java.text.MessageFormat;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
+import org.whatever.stockspider.constants.StockPrefix;
 import org.whatever.stockspider.db.entity.CompanyInfo;
-import org.whatever.stockspider.pipeline.zh.StockCodePipeline;
-import org.whatever.stockspider.processor.zh.StockCodeProcessor;
+import org.whatever.stockspider.pipeline.zh.StockHisPricePipeline;
+import org.whatever.stockspider.processor.zh.StockHisPriceProcessor;
 import org.whatever.stockspider.service.StockService;
 import org.whatever.stockspider.spider.Spiderable;
 
@@ -25,18 +27,18 @@ public class StockHisPriceSpider implements Spiderable {
     /**
      * 股票历史价格api获取页面：https://quote.eastmoney.com/sz000063.html
      */
-    private static String STOCK_HIS_PRICE_API_URL = "https://push2his.eastmoney.com/api/qt/stock/kline/get?cb=jQuery351030916149493861744_%s&secid=%s&ut=fa5fd1943c7b386f172d6893dbfba10b&fields1=f1%2Cf2%2Cf3%2Cf4%2Cf5%2Cf6&fields2=f51%2Cf52%2Cf53%2Cf54%2Cf55%2Cf56%2Cf57%2Cf58%2Cf59%2Cf60%2Cf61&klt=101&fqt=1&beg=0&start=19900101&end=20500101&smplmt=460&lmt=1000000&_=%s";
+    private static String STOCK_HIS_PRICE_API_URL = "https://push2his.eastmoney.com/api/qt/stock/kline/get?cb=jQuery351030916149493861744_{0}&secid={1}&ut=fa5fd1943c7b386f172d6893dbfba10b&fields1=f1%2Cf2%2Cf3%2Cf4%2Cf5%2Cf6&fields2=f51%2Cf52%2Cf53%2Cf54%2Cf55%2Cf56%2Cf57%2Cf58%2Cf59%2Cf60%2Cf61&klt=101&fqt=1&beg=0&start=19900101&end=20500101&smplmt=460&lmt=1000000&_={2}";
 
     @Autowired
-    private StockCodeProcessor stockCodeProcessor;
+    private StockHisPriceProcessor stockHisPriceProcessor;
     @Autowired
-    private StockCodePipeline stockCodePipeline;
+    private StockHisPricePipeline stockHisPricePipeline;
     @Autowired
     private StockService stockService;
 
     @Override
     public void run() {
-        new Spider(stockCodeProcessor).addPipeline(stockCodePipeline).addUrl(getUrls()).run();
+        new Spider(stockHisPriceProcessor).addPipeline(stockHisPricePipeline).addUrl(getUrls()).thread(8).run();
     }
 
     @Override
@@ -49,11 +51,8 @@ public class StockHisPriceSpider implements Spiderable {
     }
 
     protected String getStockPriceApiUrl(CompanyInfo companyInfo) {
-        long millis = System.currentTimeMillis();
-        return String.format(STOCK_HIS_PRICE_API_URL, millis, toMartetCode(companyInfo), millis);
+        Long millis = System.currentTimeMillis();
+        return MessageFormat.format(STOCK_HIS_PRICE_API_URL, millis.toString(), StockPrefix.toMartetCode(companyInfo.getCode()), millis.toString());
     }
 
-    protected String toMartetCode(CompanyInfo companyInfo) {
-        return companyInfo.getMarket() + "." + companyInfo.getCode();
-    }
 }
