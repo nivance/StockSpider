452a7dd4feb2b279d1f3a5f46e9f19f7ab8b2cb7
nivance <limingjian110@163.com>
2024-04-18 21:33:14 +0800

    更正取数范围

--- src/main/java/org/whatever/stockspider/spider/zh/StockHisPriceSpider.java
+++ src/main/java/org/whatever/stockspider/spider/zh/StockHisPriceSpider.java
@@ -27,7 +27,7 @@ public class StockHisPriceSpider implements Spiderable {
     /**
      * 股票历史价格api获取页面：https://quote.eastmoney.com/sz000063.html
      */
-    private static String STOCK_HIS_PRICE_API_URL = "https://push2his.eastmoney.com/api/qt/stock/kline/get?cb=jQuery351030916149493861744_{0}&secid={1}&ut=fa5fd1943c7b386f172d6893dbfba10b&fields1=f1%2Cf2%2Cf3%2Cf4%2Cf5%2Cf6&fields2=f51%2Cf52%2Cf53%2Cf54%2Cf55%2Cf56%2Cf57%2Cf58%2Cf59%2Cf60%2Cf61&klt=101&fqt=1&beg=0&start=19900101&end=20500101&smplmt=460&lmt=1000000&_={2}";
+    private static String STOCK_HIS_PRICE_API_URL = "https://push2his.eastmoney.com/api/qt/stock/kline/get?cb=jQuery351030916149493861744_{0}&secid={1}&ut=fa5fd1943c7b386f172d6893dbfba10b&fields1=f1%2Cf2%2Cf3%2Cf4%2Cf5%2Cf6&fields2=f51%2Cf52%2Cf53%2Cf54%2Cf55%2Cf56%2Cf57%2Cf58%2Cf59%2Cf60%2Cf61&klt=101&fqt=1&beg=0&start=19900101&end=20500101&smplmt=1000000&lmt=1000000&_={2}";
 
     @Autowired
     private StockHisPriceProcessor stockHisPriceProcessor;
