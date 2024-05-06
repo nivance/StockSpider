84bedf33584d65468b9c9d675b1539c5f736d8c9
nivance <limingjian110@163.com>
2024-05-06 11:06:40 +0800

    fix remove his data

--- src/main/java/org/whatever/stockspider/pipeline/zh/StockTodayPricePipeline.java
+++ src/main/java/org/whatever/stockspider/pipeline/zh/StockTodayPricePipeline.java
@@ -1,6 +1,12 @@
 package org.whatever.stockspider.pipeline.zh;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.whatever.stockspider.db.entity.DayPrice;
+import org.whatever.stockspider.service.StockService;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -13,4 +19,14 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Component
 public class StockTodayPricePipeline extends StockHisPricePipeline {
+
+    @Autowired
+    private StockService stockService;
+
+    @Transactional(rollbackFor = Exception.class)
+    void batchInsert(List<DayPrice> dayPrices) {
+        stockService.batchInsertDayPrice(dayPrices);
+    }
+
+
 }
